b1bd5bcd6d33eaf075945e1451cef29601b54f12
CrazyEagle <1251098491@qq.com>
2021-02-22 09:36:10 +0800

    网络封装初始化

---
+++ src/network/request.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+//封装请求
+export function request(config){
+  const instance=axios.create({
+    baseURL:'http://123.207.32.32:8000',
+    timeout:5000
+  })
+
+  //响应拦截
+  instance.interceptors.response.use(res=>{
+    return res.data
+  },err=>{
+    console.log(err);
+  })
+  return instance(config)
+}
